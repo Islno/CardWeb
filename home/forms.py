1d79f38988b0ba17b36f63d73c59e393fe98a66e
Islânio Pablo <islaniopbl@gmail.com>
2024-12-17 02:10:24 +0000

    .

--- home/forms.py
+++ home/forms.py
@@ -9,7 +9,10 @@ class ClienteForm(forms.ModelForm):
         widgets = {
             'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome'}),
             'cpf': forms.TextInput(attrs={'class': 'cpf form-control', 'placeholder': 'C.P.F'}),
-            'datanasc': forms.DateInput(attrs={'class': 'data form-control', 'placeholder': 'Data de Nascimento'}, format='%d/%m/%Y'),
+            'datanasc': forms.DateInput(
+                attrs={'class': 'data form-control', 'placeholder': 'Data de Nascimento'}, 
+                format='%d/%m/%Y'
+            ),
         }
         labels = {
             'nome': 'Informe o nome do cliente: ',
@@ -18,36 +21,27 @@ class ClienteForm(forms.ModelForm):
         }
 
     def clean_nome(self):
-          nome = self.cleaned_data.get('nome')
-          if len(nome) < 3:
-               raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
-          
-          # if Categoria.objects.filter(nome=nome).exists():
-          #      raise forms.ValidationError("Já existe uma categoria com esse mesmo nome.")
-          
-          return nome
-     
-     def clean_ordem(self):
-          ordem = self.cleaned_data.get('ordem')
-          if ordem <= 0:
-               raise forms.ValidationError("O campo ordem tem que ser superior a 0.")
-          
-          # if Categoria.objects.filter(ordem=ordem).exists():
-          #      raise forms.ValidationError("O númera da ordem digitada já esta sendo utilizada.")
-          
-          return ordem
+        nome = self.cleaned_data.get('nome')
+        if len(nome) < 3:
+            raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+        return nome
 
-     # def clean(self):
-     #      cleaned_data = super().clean()
-     #      nome = cleaned_data().get('nome')
-     #      ordem = cleaned.data().get('ordem')
+    def clean_ordem(self):
+        ordem = self.cleaned_data.get('ordem')
+        if ordem <= 0:
+            raise forms.ValidationError("O campo ordem tem que ser superior a 0.")
+        return ordem
 
-     #      if len(nome) < 3:
-     #           raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
-     #      return nome
+    # def clean(self):
+    #     cleaned_data = super().clean()
+    #     nome = cleaned_data.get('nome')
+    #     ordem = cleaned_data.get('ordem')
+    #     if len(nome) < 3:
+    #         raise forms.ValidationError("O nome deve ter pelo menos 3 caracteres.")
+    #     return cleaned_data
 
-     # Validação generica:
-     # def validar_valor(valor):
-     #      if len(valor) < 3:
-     #           raise forms.ValidationError("O campo deve ter pelo menos 3 caracteres.")
-     #      return valor
+    # Validação genérica:
+    # def validar_valor(valor):
+    #     if len(valor) < 3:
+    #         raise forms.ValidationError("O campo deve ter pelo menos 3 caracteres.")
+    #     return valor
