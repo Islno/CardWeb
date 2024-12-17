cf617fec86618ae0b9eef7e62019414c42a5b32f
islno <islaniopbl@gmail.com>
2024-12-16 22:30:30 -0300

    .

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Categoria
+from django.contrib import messages
 from .forms import CategoriaForm
 
 def index(request):
@@ -16,21 +17,31 @@ def form_categoria(request):
         form = CategoriaForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success('operação cadastrada com sucesso!')
             return redirect('lista')
     else: 
         form = CategoriaForm()
     return render(request, 'categoria/formulario.html', {'form': form})
 
 def editar_categoria(request, id):
-    categoria = Categoria.objects.get(id=id)
-    if request.method == 'POST':
+    try:
+        categoria = Categoria.objects.get(pk=id)
+    except:
+        messages.error(request, 'Registro não encontrado')
+        return redirect('lista')
+
+    if (request.method == 'POST'):
         form = CategoriaForm(request.POST, instance=categoria)
         if form.is_valid():
-            form.save()
-            return redirect('lista')
+            categoria = form.save()
+            lista=[]
+            lista.append(categoria)
+            return render(request, 'categoria/lista.html', {'lista':lista,})
+
     else: 
         form = CategoriaForm(instance=categoria)
-    return render(request, 'categoria/formulario.html', {'form': form})
+    
+    return render(request, 'categoria/formulario.html', {'form':form,})
 
 def delete_categoria(request, id):
     categoria = Categoria.objects.get(id=id)
