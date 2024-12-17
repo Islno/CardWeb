15a2416cd3ceb084edd6a7835f310125e4ba2c8c
islno <islaniopbl@gmail.com>
2024-12-16 22:48:15 -0300

    .

--- home/models.py
+++ home/models.py
@@ -1,9 +1,9 @@
 import locale
 from django.db import models
 
-class Categoria(models.Model): 
-    nome = models.CharField(max_length = 100)
-    ordem = models.IntegerField()
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    ordem = models.PositiveIntegerField(unique=True)
 
     def __str__(self):
         return self.nome
